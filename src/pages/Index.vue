15a33fe5c47adffd063ee7031ddc747d13b5d358
M4cr0s3 <tmmllllllllll@gmail.com>
2025-05-05 20:05:27 +0300

    feat: changed index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
+import {Button} from "@/components/ui";
+import {useRouter} from "vue-router";
+import {ROUTES} from "@/router/routes.ts";
+import {useUsersStore} from "@/stores";
+import {computed} from "vue";
 
+const router = useRouter();
+const usersStore = useUsersStore();
+
+const text = computed(() => {
+  return usersStore.currentUser?.name ? 'панели' : 'входа';
+});
 </script>
 
 <template>
-  {{ $route }}
-  Index page
+  <div class="flex items-center justify-center min-h-screen">
+    <div class="flex flex-col items-center space-y-3">
+      <h1 class="text-3xl font-bold text-gray-700">
+        Перейти на страницу {{ text }}
+      </h1>
+      <Button @click="router.push(text === 'панели' ? ROUTES.DASHBOARD : ROUTES.LOGIN)">
+        На страницу {{ text }}
+      </Button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
