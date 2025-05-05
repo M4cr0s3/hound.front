24e5bdd09bb460746bf0f1f6ff77e58e23f4b01f
M4cr0s3 <tmmllllllllll@gmail.com>
2025-05-05 22:49:16 +0300

    fix(issues): deleted assignees and tags temporarily

--- src/components/issue/IssuesTable.vue
+++ src/components/issue/IssuesTable.vue
@@ -36,24 +36,25 @@
         {{ value }}
       </span>
     </template>
-    <template #cell-assignees="{ value }">
-      <span v-if="value && value.length">
-        {{ value.map((user: User) => user.name).join(', ') }}
-      </span>
-      <span v-else class="text-gray-500">Не назначено</span>
-    </template>
-    <template #cell-tags="{ value }">
-      <span v-if="value && value.length" class="flex gap-1">
-        <span
-            v-for="tag in value"
-            :key="tag"
-            class="bg-gray-200 text-gray-700 px-2 py-1 rounded-full text-xs"
-        >
-          {{ tag }}
-        </span>
-      </span>
-      <span v-else class="text-gray-500">Нет тегов</span>
-    </template>
+<!--    <template #cell-assignees="{ value }">-->
+<!--      {{ value }}-->
+<!--      <span v-if="value && value.length">-->
+<!--        {{ value.map((user: User) => user.name).join(', ') }}-->
+<!--      </span>-->
+<!--      <span v-else class="text-gray-500">Не назначено</span>-->
+<!--    </template>-->
+<!--    <template #cell-tags="{ value }">-->
+<!--      <span v-if="value && value.length" class="flex gap-1">-->
+<!--        <span-->
+<!--            v-for="tag in value"-->
+<!--            :key="tag"-->
+<!--            class="bg-gray-200 text-gray-700 px-2 py-1 rounded-full text-xs"-->
+<!--        >-->
+<!--          {{ tag }}-->
+<!--        </span>-->
+<!--      </span>-->
+<!--      <span v-else class="text-gray-500">Нет тегов</span>-->
+<!--    </template>-->
     <template #cell-due_date="{ value }">
       <span v-if="value" class="text-gray-500 flex items-center">
         <Icon icon="heroicons-outline:calendar" class="h-4 w-4 mr-2 inline-block"/>
@@ -67,7 +68,7 @@
 import {computed} from 'vue';
 import {Icon} from '@iconify/vue';
 import {DataTable} from '@/components/ui';
-import {type Issue, IssueStatus, IssuePriority, type User, type Pagination} from '@/api';
+import {type Issue, IssueStatus, IssuePriority, type Pagination} from '@/api';
 
 const props = defineProps<{
   issues: Issue[];
@@ -85,8 +86,6 @@ const columns = computed(() => [
   {key: 'title', title: 'Название'},
   {key: 'status', title: 'Статус'},
   {key: 'priority', title: 'Приоритет'},
-  {key: 'assignees', title: 'Назначено'},
-  {key: 'tags', title: 'Теги'},
   {key: 'due_date', title: 'Срок',},
 ]);
 
