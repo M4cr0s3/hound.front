088dda92b45f64af84e0f84c43150aa721ec628d
krupi <krupin_2004@bk.ru>
2025-05-28 10:29:07 +0300

    fix: types errors on event

--- src/stores/events.ts
+++ src/stores/events.ts
@@ -1,6 +1,6 @@
-import {defineStore} from "pinia";
-import {ref} from "vue";
-import {type Event, httpClient, type Id} from "@/api";
+import { type Event, httpClient, type Id, type Issue } from "@/api";
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
 export const useEventsStore = defineStore('events', () => {
   const events = ref<Event[]>([]);
@@ -18,6 +18,7 @@ export const useEventsStore = defineStore('events', () => {
     project_id: 0,
     environment: '',
     project: {} as Event['project'],
+    issues: {} as Issue[]
   });
   const isLoading = ref(false);
 
