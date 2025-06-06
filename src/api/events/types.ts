1d54dc878ab4105a8f2dfa49ecaffd92e9028003
krupi <krupin_2004@bk.ru>
2025-06-06 10:47:50 +0300

    fix: type error on issue

--- src/api/events/types.ts
+++ src/api/events/types.ts
@@ -6,7 +6,7 @@ export interface Event {
   project_id: number;
   environment: string;
   type: string;
-  level: 'error' | 'warning' | 'info' | 'debug';
+  level: 'error' | 'warning' | 'info' | 'debug' | 'critical';
   message: string;
   release: string | null;
   metadata: {
